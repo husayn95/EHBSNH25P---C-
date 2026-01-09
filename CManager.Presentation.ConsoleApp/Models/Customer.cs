9fffb81142b337a7c62f1bdf7a850d6e6be9f17c
husayn95 <husan254@gmail.com>
2026-01-09 16:08:56 +0500

    customer modellen

---
+++ CManager.Presentation.ConsoleApp/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace CManager.Presentation.ConsoleApp.Models
+{
+    // Customer representerar en kund i systemet.
+    // Klassen används som en datamodell (Model).
+    public class Customer
+    {
+        // Unikt ID för varje kund
+        public Guid Id { get; set; }
+
+        // Kundens förnamn
+        public string FirstName { get; set; } = null!;
+
+        // Kundens efternamn
+        public string LastName { get; set; } = null!;
+
+        // Kundens e-postadress ( för att identifiera kunden)
+        public string Email { get; set; } = null!;
+
+        // kundens telefonnummer
+        public string Phone { get; set; } = null!;
+
+        // Kundens adress 
+        public Address Address { get; set; } = null!;
+    }
+
+}
