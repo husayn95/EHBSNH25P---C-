fa3aad58eb8fd51f93de2c207d72ec1c993bc89a
husayn95 <husan254@gmail.com>
2026-01-10 17:18:36 +0500

    service hantering färdig

---
+++ CManager.Presentation.ConsoleApp/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Presentation.ConsoleApp.Controllers
+{
+
+    // Controller ansvarar för användarens interaktion via konsolen
+    public class CustomerController
+    {
+        private readonly ICustomerService _service;
+
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
+
+  
+}
---
+++ CManager.Presentation.ConsoleApp/Services/ICustomerService.cs
@@ -0,0 +1,23 @@
+﻿using CManager.Presentation.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace CManager.Presentation.ConsoleApp.Services
+{
+    public interface ICustomerService
+    {
+        //skapar en ny kund
+        Customer CreateCustomer(string firstName, string lastName, string email, string phone, Address address);
+
+        //sämtar alla kunder
+        List<Customer> GetAll();
+
+        //sämtar en specifik kund baserat på e-post
+        Customer? GetByEmail(string email);
+
+        //tar bort en kund baserat på e-post
+        bool DeleteByEmail(string email);
+    }
+}
